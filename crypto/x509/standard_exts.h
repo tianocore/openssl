f80ece5971a1dbbe227618c38ee5df2be89613db
Yi Li <yi1.li@intel.com>
2023-03-01 11:33:16 +0800

    x509: remove unused extentions
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- crypto/x509/standard_exts.h
+++ crypto/x509/standard_exts.h
@@ -13,6 +13,7 @@
  */
 
 static const X509V3_EXT_METHOD *standard_exts[] = {
+#ifndef OPENSSL_SYS_UEFI
     &ossl_v3_nscert,
     &ossl_v3_ns_ia5_list[0],
     &ossl_v3_ns_ia5_list[1],
@@ -21,6 +22,7 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
     &ossl_v3_ns_ia5_list[4],
     &ossl_v3_ns_ia5_list[5],
     &ossl_v3_ns_ia5_list[6],
+#endif
     &ossl_v3_skey_id,
     &ossl_v3_key_usage,
     &ossl_v3_pkey_usage_period,
@@ -37,8 +39,10 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
 #ifndef OPENSSL_NO_OCSP
     &ossl_v3_crl_invdate,
 #endif
+#ifndef OPENSSL_SYS_UEFI
     &ossl_v3_sxnet,
     &ossl_v3_info,
+#endif
 #ifndef OPENSSL_NO_RFC3779
     &ossl_v3_addr,
     &ossl_v3_asid,
@@ -51,11 +55,14 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
     &ossl_v3_ocsp_acutoff,
     &ossl_v3_ocsp_serviceloc,
 #endif
+#ifndef OPENSSL_SYS_UEFI
     &ossl_v3_sinfo,
     &ossl_v3_policy_constraints,
+#endif
 #ifndef OPENSSL_NO_OCSP
     &ossl_v3_crl_hold,
 #endif
+#ifndef OPENSSL_SYS_UEFI
     &ossl_v3_pci,
     &ossl_v3_name_constraints,
     &ossl_v3_policy_mappings,
@@ -63,15 +70,18 @@ static const X509V3_EXT_METHOD *standard_exts[] = {
     &ossl_v3_idp,
     &ossl_v3_alt[2],
     &ossl_v3_freshest_crl,
+#endif
 #ifndef OPENSSL_NO_CT
     &ossl_v3_ct_scts[0],
     &ossl_v3_ct_scts[1],
     &ossl_v3_ct_scts[2],
 #endif
+#ifndef OPENSSL_SYS_UEFI
     &ossl_v3_utf8_list[0],
     &ossl_v3_issuer_sign_tool,
     &ossl_v3_tls_feature,
     &ossl_v3_ext_admission
+#endif
 };
 
 /* Number of standard extensions */
