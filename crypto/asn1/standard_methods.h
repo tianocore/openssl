a2a812f9bf367fc3d399f35b94ab33719ee48dd3
Yi Li <yi1.li@intel.com>
2023-03-21 14:09:25 +0800

    ec: disable ecx and ecd
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- crypto/asn1/standard_methods.h
+++ crypto/asn1/standard_methods.h
@@ -32,14 +32,14 @@ static const EVP_PKEY_ASN1_METHOD *standard_methods[] = {
 #ifndef OPENSSL_NO_DH
     &ossl_dhx_asn1_meth,
 #endif
-#ifndef OPENSSL_NO_EC
-    &ossl_ecx25519_asn1_meth,
-    &ossl_ecx448_asn1_meth,
-#endif
-#ifndef OPENSSL_NO_EC
-    &ossl_ed25519_asn1_meth,
-    &ossl_ed448_asn1_meth,
-#endif
+// #ifndef OPENSSL_NO_EC
+//     &ossl_ecx25519_asn1_meth,
+//     &ossl_ecx448_asn1_meth,
+// #endif
+// #ifndef OPENSSL_NO_EC
+//     &ossl_ed25519_asn1_meth,
+//     &ossl_ed448_asn1_meth,
+// #endif
 #ifndef OPENSSL_NO_SM2
     &ossl_sm2_asn1_meth,
 #endif
